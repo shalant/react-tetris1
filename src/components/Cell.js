b8268523ff1270e1d71784090640d9ffa1a359b4
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-01-10 10:28:58 -0600

    working on styles, amazing props and calculations

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <div>
+    <StyledCell type={'L'} color={TETROMINOS['L'].color}>
         Cell 
-    </div>
+    </StyledCell>
 )
     
 
 
-export default Cell
+export default Cell;
