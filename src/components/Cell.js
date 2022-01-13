8842838818e84a00f656fdcabbab9f328cb3c539
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-01-13 16:19:37 -0600

    finished, cool video!

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,6 +3,8 @@ import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <StyledCell type={type} color={TETROMINOS[type].color} />);
+    <StyledCell type={type} color={TETROMINOS[type].color}>{console.log('rerender')}</StyledCell>
+);
 
-export default Cell;
+// memoizes each event and only re-renders when the cells are changing
+export default React.memo(Cell);
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -20,6 +20,7 @@ const Tetris = () => {
 
     const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
     const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+    const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared);
 
     console.log('re-render');
 
@@ -33,11 +34,21 @@ const Tetris = () => {
         console.log('test')
         // Reset everything
         setStage(createStage());
+        setDropTime(1000);
         resetPlayer();
         setGameOver(false);
+        setScore(0);
+        setRows(0);
+        setLevel(0);
     }
 
     const drop = () => {
+        //Increase level when player has cleared 10 rows
+        if (rows > (level + 1) * 10) {
+            setLevel(prev => prev + 1);
+            //Also increase speed
+            setDropTime(1000 / (level + 1) + 200);
+        }
         if (!checkCollision(player, stage, { x: 0, y: 1 })) {
             updatePlayerPos({ x: 0, y: 1, collided: false})
         } else {
@@ -51,7 +62,19 @@ const Tetris = () => {
         }
     }
 
+    const keyUp = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 40) {
+                console.log('interval on')
+
+                setDropTime(1000 / (level + 1) + 200);
+            }
+        }
+    }
+
     const dropPlayer = () => {
+        console.log('interval off')
+        setDropTime(null);
         drop();
     }
 
@@ -73,11 +96,16 @@ const Tetris = () => {
         }
     };
 
+    useInterval(() => {
+        drop();
+    }, dropTime);
+
     return (
         <StyledTetrisWrapper 
             role='button' 
             tabIndex='0' 
             onKeyDown={e => move(e)}
+            onKeyUp={keyUp}
         >
             <StyledTetris>
                 <Stage stage={stage} />
@@ -86,9 +114,9 @@ const Tetris = () => {
                         <Display gameOver={gameOver} text='Game Over' />
                     ) : (
                         <div>
-                            <Display text="Score" />
-                            <Display text="Rows" />
-                            <Display text="Level" />
+                            <Display text={`Score: ${score}`} />
+                            <Display text={`Rows: ${rows}`} />
+                            <Display text={`Level: ${level}`} />
                         </div>
                     )}
                     <StartButton callback={startGame} />
